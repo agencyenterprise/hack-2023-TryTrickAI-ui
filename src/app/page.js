657e01449d88056c5a25390caf25cc85315179c4
rhinoandre <rhinoandre@gmail.com>
2023-09-23 15:55:08 -0300

    feat: add logo

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,11 @@ import { Divider } from "@/components/Divider";
 export default function Home() {
   return (
     <>
-      <h1 className="text-8xl leading-snug text-center">Try to<br />Trick AI</h1>
-      <div className="rounded-sm space-y-8 text-left p-8 bg-card shadow-xl border border-white/50 text-black">
+      <Header />
+      <div className="rounded-sm space-y-8 text-left p-8 bg-card shadow-xl border border-white/50 text-black max-w-2xl mx-auto">
         <Divider color="bg-[#292929]" />
         <h2 className="text-2xl font-medium mt-10 mb-6 text-center">How it works</h2>
-        <p className="font-medium max-w-3xl text-center">Your mission is to outwit the cunning AI, lurking in the digital shadows, by crafting a cryptic clue that will leave it baffled, while simultaneously allowing your fellow human players to decipher the secret concealed within. It’s humans against the machine! Welcome to the world of intrigue and deception. Attempt to solve a puzzle to begin:</p>
+        <p className="font-medium text-center">Your mission is to outwit the cunning AI, lurking in the digital shadows, by crafting a cryptic clue that will leave it baffled, while simultaneously allowing your fellow human players to decipher the secret concealed within. It’s humans against the machine! Welcome to the world of intrigue and deception. Attempt to solve a puzzle to begin:</p>
         <Divider color="bg-[#292929]" />
       </div>
 
@@ -18,4 +18,7 @@ export default function Home() {
       </div>
     </>
   )
+}
+function Header() {
+  return <img className="mx-auto mt-24 mb-5 ml-[90px] -mb-[100px]" src="/logo.svg" />
 }
