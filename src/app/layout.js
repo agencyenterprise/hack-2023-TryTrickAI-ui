05b009bd1ef679b0ad869e2464e0b0ae61f2235b
rhinoandre <rhinoandre@gmail.com>
2023-09-23 11:08:29 -0300

    fix: background gradient

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,12 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className="bg-background"
-        suppressHydrationWarning
-      >
+      <body className='bg-background' suppressHydrationWarning>
+        <span className='fixed inset-0 bg-[url(/background.png)] bg-cover bg-center bg-black -z-[11]' />
         <Menu />
-        <main className="grid place-content-center min-h-screen bg-[url(/background.png)] bg-cover bg-center pb-5">
+        <main className="grid place-content-center min-h-screen pb-5">
           {children}
         </main>
       </body>
